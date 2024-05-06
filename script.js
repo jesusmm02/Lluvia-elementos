1c97f8828874632a2ea85583d9c6bc3ad2e47594
jmelara02 <jesusmelara77@gmail.com>
2024-05-06 19:54:08 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const body = document.querySelector("body");
+
+function createFallingElement() {
+  let element = document.createElement("i"); 
+  element.className = "copo";
+  element.style.left = Math.random() * innerWidth + "px"; // Posición inicial horizontal aleatoria
+  
+  let size = (Math.random() * 8) + 2;
+  element.style.width = size + "px";
+  element.style.height = size + "px";
+
+  let delay = Math.random() * 5;
+  element.style.animationDelay = delay + "s";
+  
+  let anima = (Math.random() * 10) + 5;
+  element.style.animationDuration = anima + "s";
+  
+  body.appendChild(element);
+
+  setTimeout(() => {
+    element.remove(); // Elimina el elemento después de cierto tiempo
+  }, anima * 1000);
+}
+
+setInterval(createFallingElement, 50);
